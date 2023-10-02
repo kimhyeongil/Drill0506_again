98c0296dc090ac3420cd06ca99ff5d6cee419b4a
KHI(home) <alfkcjstk853@naver.com>
2023-10-02 17:18:25 +0900

    캐릭터가 손화살표를 추적

--- Drill05.py
+++ Drill05.py
@@ -27,12 +27,15 @@ def handle_events():
 def reset_world():
     global running, cx, cy, frame
     global hx, hy
-
+    global sx, sy
+    global t
     running = True
     cx, cy = TUK_WIDTH // 2, TUK_HEIGHT // 2
     frame = 0
 
-    hx, hy = random.randint(0, TUK_WIDTH), random.randint(0, TUK_HEIGHT)
+    sx, sy = cx, cy  # p1 : 시작점
+    hx, hy = random.randint(0, TUK_WIDTH), random.randint(0, TUK_HEIGHT)  # p2 : 끝점
+    t = 0.0
     # hx, hy = TUK_WIDTH - 50, TUK_HEIGHT - 50
 
 
@@ -46,7 +49,14 @@ def render_world():
 
 def update_world():
     global frame
+    global cx, cy
+    global t
+
     frame = (frame + 1) % 8
+    if t <= 1.0:
+        cx = (1 - t) * sx + t * hx  # cx는 시작 x와 끝 x를 1-t:t의 비율로 섞은 위치
+        cy = (1 - t) * sy + t * hy
+        t += 0.001
 
 
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
